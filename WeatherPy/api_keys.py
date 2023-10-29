3c49f41eb3f3b1a687d00499e8e1a3a50841e5ae
tiffphi <144742702+tiffphi@users.noreply.github.com>
2023-10-29 08:39:57 -0400

    Update api_keys.py
    manually removed key's

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "da8f247969069a93a35dad14b8f9712e"
+weather_api_key = "YOUR KEY CODE"
 
 # Geoapify API Key
-geoapify_key = "46ddd7fec3e249099fa606f61a033310"
+geoapify_key = "YOUR KEY CODE"
